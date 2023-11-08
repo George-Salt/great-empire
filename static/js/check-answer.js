d7284760a69f9f93e668409cc37ad72d23a286b7
George <solanikg3@gmail.com>
2023-11-08 10:58:26 +0300

    Init in test mode

---
+++ static/js/check-answer.js
@@ -0,0 +1,20 @@
+let validAnswerContainer = document.querySelector("main > .task .valid");
+let answers = document.querySelectorAll("main > .task .answer");
+let button = document.querySelector("main > .task .button");
+let solution = document.querySelector("main > .task .solution");
+
+
+function checkAnswer() {
+  button.remove()
+  solution.style.display = "block";
+  validAnswerContainer.classList.add("answered");
+
+  answers.forEach((element) => {
+    if (element.querySelector("input").checked == true && !element.classList.contains("answered")) {
+      element.classList.add("invalid");
+    };
+
+    element.querySelector("input").checked = false;
+    element.querySelector("input").disabled = true;
+  });
+};
